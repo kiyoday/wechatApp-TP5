971b18d314e8d6248832f33baa578cba8f8a1846
kiyoday <1260583285@qq.com>
2020-05-24 18:55:38 +0800

    category:model&controller

--- application/api/controller/V1/Category.php
+++ application/api/controller/V1/Category.php
@@ -3,10 +3,17 @@
 
 namespace app\api\controller\V1;
 
+use app\api\model\Category as CategoryModel;
+use app\lib\exception\CategoryException;
 
 class Category
 {
-    public function getAllCategory(){
-
+    public function getAllCategories(){
+        $categories = CategoryModel::with('img')
+            ->select();
+        if($categories->isEmpty()){
+            throw new CategoryException();
+        }
+        return $categories;
     }
 }
--- application/api/model/Category.php
+++ application/api/model/Category.php
@@ -6,5 +6,10 @@ namespace app\api\model;
 
 class Category extends BaseModel
 {
+    protected $hidden = ['delete_time',
+        'create_time','update_time'];
 
+    public function img(){
+        return $this->belongsTo('Image','topic_img_id','id');
+    }
 }
