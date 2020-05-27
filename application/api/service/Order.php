ca0379bfe894c6b4e94b6339ce1aa182159abcc8
kiyoday <1260583285@qq.com>
2020-05-27 11:31:22 +0800

    order snap

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -5,7 +5,9 @@ namespace app\api\service;
 
 
 use app\api\model\Product;
+use app\api\model\UserAddress;
 use app\lib\exception\OrderException;
+use app\lib\exception\UserException;
 
 class Order
 {
@@ -21,6 +23,47 @@ class Order
         $this->oProducts = $oProducts;
         $this->products = $this->getProductsByOrder($oProducts);
         $this->uid = $uid;
+        $status = $this->getOrderStatus();
+        if(!$status['pass']){
+            $status['order_id'] = -1;
+            return $status;
+        }
+        //开始创建订单
+        $orderSnap = $this->snapOrder();
+    }
+
+    //生成订单快照
+    private function snapOrder($status){
+        $snap = [
+            'orderPrice' => 0,
+            'totalCount' => 0,
+            'pStatus' => [],
+            'snapAddress' => '',
+            'snapName' => '',
+            'snapImg' => '',
+        ];
+        $snap['orderPrice'] = $status['orderPrice'];
+        $snap['totalCount'] = $status['totalCount'];
+        $snap['pStatus'] = $status['pStatusArray'];
+        $snap['snapAddress'] = json_encode($this->getUserAddress());
+        $snap['snapName'] = $this->products[0]['name'];
+        $snap['snapImg'] = $this->products[0]['main_img_url'];
+
+        if(count($this->products) > 1){
+            $snap['snapName'] .= '等';
+        }
+    }
+
+    private function getUserAddress(){
+        $userAddress = UserAddress::where('user_id','=',$this->uid)
+            ->find();
+        if(!$userAddress){
+            throw new UserException([
+                'msg' => '用户收货地址不存在，下单失败',
+                'errorCode' => 60001,
+            ]);
+        }
+        return $userAddress->toArray();
     }
 
     //获取订单状态
@@ -28,6 +71,7 @@ class Order
         $status = [
             'pass' => true,
             'orderPrice' => 0,
+            'totalCount' => 0,
             'pStatusArray' => [],//保存订单中所有商品的详细信息
         ];
 
@@ -39,6 +83,7 @@ class Order
                 $status['pass'] = false;
             }
             $status['orderPrice'] += $pStatus['totalPrice'];
+            $status['totalCount'] += $pStatus['count'];
             array_push($status['pStatusArray'], $pStatus);
         }
         return $status;
